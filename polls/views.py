fc244abd22fa8e49e256c379de9524ef598b4159
saurav.kumar <saurav.kumar@edcellence.in>
2013-11-29 20:04:49 +0530

    A working poll app

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 # Create your views here.
-from django.http import HttpResponse
-from polls.models import Poll
+from django.http import HttpResponse, HttpResponseRedirect
+from polls.models import Poll, Choice
+from django.template import RequestContext # Added in part 4 for vote()
+from django.core.urlresolvers import reverse # Added in part 4 for vote()
 #from django.template import Context, loader
+
 
 from django.shortcuts import render_to_response, get_object_or_404
 #Note no need to import Context and loader after importing this.
@@ -40,12 +43,37 @@ def detail(request, poll_id):
     
     return render_to_response('polls/detail.html', {
         'poll': p,
-        'User': 'Saurav Kumar'
-        })
-    '''Give special attention to this function. Get help from index() above'''
+        'User': 'Saurav Kumar',
+        },
+        context_instance=RequestContext(request))
+    '''Give special attention to this function. Get help from index() how
+    previous template and context are used now '''
 
-def vote(request, choice_id):
-    return 'Hello!'
+def vote(request, poll_id):
+    ''' this function is used to increment the vote count for Poll's selected choice'''
+    
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        '''Redisplay the poll voting form.'''
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'User': 'Saurav Kumar',
+            'error_message': "Please select a choice first.",
+            }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+
 
 def results(request, poll_id):
-    return 'Hello!'
+    p = get_object_or_404(Poll, pk = poll_id)
+    return render_to_response('polls/results.html', {
+        'poll': p,
+        'User': 'Saurav Kumar',
+        })
