916b0026d6922bc4f803e64197c66ce3632ee625
saurav.kumar <saurav.kumar@edcellence.in>
2013-11-30 12:57:10 +0530

    Used Generic View, completion of Django Tutorial

--- polls/views.py
+++ polls/views.py
@@ -11,43 +11,8 @@ from django.shortcuts import render_to_response, get_object_or_404
 #thus we can return the page like:
 # return render_to_response('path_to_template', {context}) ; see below
 
-def index(request):
-    ''' The index view, which will show all the polls'''
-
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    '''now it contains first 5 latest poll.'''
-
-    '''t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-        'User': 'Saurav Kumar'
-    })
-
-    return HttpResponse(t.render(c))
-    '''
-
-    return render_to_response('polls/index.html', {
-        'latest_poll_list': latest_poll_list,
-        'User': 'Saurav Kumar'
-        })
-
-    #output = ', '.join([p.question for p in latest_poll_list])
-    #return HttpResponse(output)
 
 
-def detail(request, poll_id):
-    '''This function is to display the details of the indivisual polls'''
-    
-    p = get_object_or_404(Poll, pk = poll_id)
-    ''' This method will get the object(Poll) bases on the its id'''
-    
-    return render_to_response('polls/detail.html', {
-        'poll': p,
-        'User': 'Saurav Kumar',
-        },
-        context_instance=RequestContext(request))
-    '''Give special attention to this function. Get help from index() how
-    previous template and context are used now '''
 
 def vote(request, poll_id):
     ''' this function is used to increment the vote count for Poll's selected choice'''
@@ -68,12 +33,4 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
-
-
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk = poll_id)
-    return render_to_response('polls/results.html', {
-        'poll': p,
-        'User': 'Saurav Kumar',
-        })
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
