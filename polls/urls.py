98d6542fe0649e45f1792ce3b968e4fbf29d4a89
saurav.kumar <saurav.kumar@edcellence.in>
2013-11-30 12:25:28 +0530

    Django Tutorial Part 4 Before Generic Views

--- polls/urls.py
+++ polls/urls.py
@@ -6,30 +6,23 @@ from polls.models import Poll
 from django.contrib import admin
 admin.autodiscover()
 
-'''
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'newsite.views.home', name='home'),
-    # url(r'^newsite/', include('newsite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
 )
-'''
-
-urlpatterns = patterns('polls.views', 
-    url(r'^$', 'index'),
-    url(r'^(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-    url(r'(?P<poll_id>\d+)/vote/$', 'vote'),
-)   # no need to append polls.views now in url()
 
 urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
