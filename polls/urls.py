48551de8e9ae950d37bcc92f6d0a49788498e655
saurav.kumar <saurav.kumar@edcellence.in>
2013-11-30 12:35:11 +0530

    Django Tutorial Part 4 Before Generic Views

--- polls/urls.py
+++ polls/urls.py
@@ -6,23 +6,12 @@ from polls.models import Poll
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$',
-        ListView.as_view(
-            queryset=Poll.objects.order_by('-pub_date')[:5],
-            context_object_name='latest_poll_list',
-            template_name='polls/index.html')),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='polls/detail.html')),
-    url(r'^(?P<pk>\d+)/results/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='polls/results.html'),
-        name='poll_results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-)
+urlpatterns = patterns('polls.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'(?P<poll_id>\d+)/vote/$', 'vote'),
+)   # no need to append polls.views now in url()
 
 urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
